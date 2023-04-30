4fb45b232f7c91973726272438aecc582a6ed00b
bhumika <bhumikabhadani69@gmail.com>
2023-04-30 11:18:19 +0100

    file add

---
+++ src/PracticeDemo.java
@@ -0,0 +1,9 @@
+public class PracticeDemo {
+
+    static String name = "I want to get £200, so that I can buy my $item";
+    public static void main(String[] args){
+        System.out.println(name.replaceAll("[^0-9]", ""));
+        System.out.println(name.replaceAll("[^a-zA-Z]",""));
+        System.out.println(name.replaceAll("[a-zA-Z\\d, ]",""));
+    }
+}
